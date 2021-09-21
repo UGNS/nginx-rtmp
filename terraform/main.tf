a7a52825009b0fbd54191397253e3e74196455c5
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2021-09-21 19:41:30 -0400

    Work to remove remote state dependency

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,30 @@
 data "aws_region" "current" {}
 
+data "aws_vpc" "current" {
+    name = var.vpc_name
+}
+
 module "rtmp_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
   name            = "rtmp"
   use_name_prefix = true
-  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id          = data.aws_vpc.current.id
   description     = "Security group for usage with NLB"
 
   ingress_with_cidr_blocks = [
     {
-      from_port   = 1935
-      to_port     = 1935
+      from_port   = var.rtmp_port
+      to_port     = var.rtmp_port
       protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
     }
   ]
   ingress_with_ipv6_cidr_blocks = [
     {
-      from_port        = 1935
-      to_port          = 1935
+      from_port        = var.rtmp_port
+      to_port          = var.rtmp_port
       protocol         = "tcp"
       ipv6_cidr_blocks = "::/0"
     }
